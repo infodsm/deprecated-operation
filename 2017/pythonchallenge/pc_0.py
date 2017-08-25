24867968732a4202bc3f3ef52583f8f8dab85bda
infreljs <infreljs@infreljsui-MBP.connectify>
2017-08-25 10:06:46 +0900

    2학기 시작

--- 2017/pythonchallenge/pc_0.py
+++
@@ -1,6 +0,0 @@
-# Python Challenge No.0
-
-url = "http://www.pythonchallenge.com/pc/def/0.html";
-url = url.replace("0", str(pow(2,38)));
-print(url);
-# http://www.pythonchallenge.com/pc/def/274877906944.html
